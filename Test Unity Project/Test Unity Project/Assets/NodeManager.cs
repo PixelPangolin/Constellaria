e3900943188116eb474aac4f2b9a526bb1a5d94c
alexczeto <alexczeto@yahoo.ca>
2018-01-29 14:54:01 -0700

    Lines created when two nodes touched

---
+++ Test Unity Project/Test Unity Project/Assets/NodeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeManager : MonoBehaviour {
+
+	public GameObject currentNode;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setCurrent(GameObject node){
+		this.currentNode = node;
+	}
+
+	public GameObject getCurrent(){
+		return this.currentNode;
+	}
+}
