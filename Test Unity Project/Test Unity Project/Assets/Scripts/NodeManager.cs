3ae3ce4685fb74c39b4cd485e00a411cce2f5cc3
D'Arcy <dmhamilt@ualberta.ca>
2018-01-31 13:45:18 -0700

    Made the line visible, added background sound
    Specifically, added a line renderer to the hero
    added a draw line clause to the update function
    added Loic's demo background sound, and made it auidible

--- Test Unity Project/Test Unity Project/Assets/Scripts/NodeManager.cs
+++ Test Unity Project/Test Unity Project/Assets/Scripts/NodeManager.cs
@@ -6,6 +6,8 @@ public class NodeManager : MonoBehaviour {
 
 	public GameObject currentNode;
 	public DistanceJoint2D hook;
+	public bool connected = false;
+	public LineRenderer line;
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +28,13 @@ public class NodeManager : MonoBehaviour {
 			print("down arrow key is held down");
 		}
 			
+		if (connected) 
+		{
+			line.SetPosition(0, transform.position);
+			GameObject node = getCurrent();
+			line.SetPosition(1, node.transform.position);
+		}
+
 	}
 
 	public void setCurrent(GameObject node){
@@ -41,6 +50,13 @@ public class NodeManager : MonoBehaviour {
 		hook.enabled = true;
 		//hook.anchor = node.transform.position;
 		hook.connectedBody = node.GetComponent<Rigidbody2D>();
+
+		//line.SetVertexCount(2); //may need this later for handling corners
+		//no such thing as a 'get list/count of vertexes', so we will need to keep a list
+		line.SetPosition(0, transform.position);
+		line.SetPosition(1, node.transform.position);
+		//line.SetWidth(0.1f, 0.1f);
+		connected = true;
 
 		// If we ever want to set the distance of the grappling hook
 		hook.distance = Vector2.Distance(transform.position,node.transform.position);
