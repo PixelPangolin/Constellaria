9ec117546e57efd48db39564019807a87a6dd777
D'Arcy <dmhamilt@ualberta.ca>
2018-02-21 01:05:48 -0700

    TwoWayLines fix
    Changed the prefab beamPivot:
    -changed it to the 'Lines' layer
    -Changed boxcollider into BoxCollider2D
    -Added PlatformEffector2D
    
    Reworked the creation script for the generated lines
    -Moved most transforms from the Update function to the Startfunction
    -Changed LookAt to a system using the principles of tan(radian) = y-y/x-x
    -added nest of if statements to ensure the correct side is always pointed up
    -disabled lines with an angle sharper than45 degrees, converting them into triggers for later use as ladders(unimplemented)
    
    Changed TwoWayLine.cs to better support the instantiated lines

--- Assets/Scripts/TwoWayLine.cs
+++ Assets/Scripts/TwoWayLine.cs
@@ -4,35 +4,44 @@ using System.Collections;
 
 public class TwoWayLine : MonoBehaviour {
 
-	public Transform groundCheck;
-
-	private bool onLine = false;
-	private BoxCollider2D boxCollider;
+	//TODO: Make this only disable the line you are standing on, for as long as you are touching it.
 
 
+	GameObject groundCheck;
+	private bool grounded = false;
+	//private BoxCollider2D boxCollider;
+
+	//public Transform groundCheck;
 	void Awake () 
 	{
-		boxCollider = GameObject.Find("hero").GetComponent<BoxCollider2D>();
+		groundCheck = GameObject.Find("groundCheck");
+		//boxCollider = GameObject.Find("hero").GetComponent<BoxCollider2D>();
 	}
 
-	// Update is called once per frame
-	void Update () 
+	// FixedUpdate is called once per physics update
+	void FixedUpdate () 
 	{
-		onLine = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Lines"));
+		grounded = (Physics2D.Linecast(transform.position, groundCheck.transform.position, 1 << LayerMask.NameToLayer("Lines")) || Physics2D.Linecast(transform.position, groundCheck.transform.position, 1 << LayerMask.NameToLayer("Ground"))) ;
 
-		if (Input.GetKey("down") && onLine) 
+		if (grounded) 
+		{
+			this.GetComponent<BoxCollider2D> ().enabled = true;
+		}
+
+		if (Input.GetKey("down")||Input.GetKey("s")) 
 		{
 			StartCoroutine("Fall");
 		}
+			
 	}
 
 	// Makes the character fall by disabling then enabling the collider
 	private IEnumerator Fall() {
-		boxCollider.isTrigger = true;
+		this.GetComponent<BoxCollider2D>().enabled = false;
 		yield return new WaitForSeconds(0.2f);
-		if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Lines")))
+		if (grounded)
 		{
-			boxCollider.isTrigger = false;
+		this.GetComponent<BoxCollider2D>().enabled = true;
 		}
 
 	}
