7087579c056e32236f91946fe2b3bb047c291c5a
D'Arcy <dmhamilt@ualberta.ca>
2018-02-22 18:35:12 -0700

    Revert "Added spike prefab and killplayerscript"
    This reverts commit a780ea05eb6b7d84bb7438b359ebfdc3569025d7.

--- Assets/Scripts/DemoReset.cs
+++ Assets/Scripts/DemoReset.cs
@@ -16,8 +16,7 @@ public class DemoReset : MonoBehaviour {
 
 		void OnTriggerEnter2D (Collider2D other)
 		{
-			if (other.gameObject.CompareTag ("Player")) {
-				Application.LoadLevel (Application.loadedLevel);
-			}
+			if (other.gameObject.CompareTag("Player"))
+				Application.LoadLevel(Application.loadedLevel);
+		}
 	}
-}
