06d5ea8d24b02caa2f2553a70018aaeb0ad56827
Sonia Budac <budac@ualberta.ca>
2018-03-19 09:50:47 -0600

    Opening Cutscene in progress
    New scene called "Opening Cutscene" with the opening in progress

---
+++ Assets/OpeningCutscene.cs
@@ -0,0 +1,21 @@
+﻿//This script will change the animator at set times
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningCutscene : MonoBehaviour {
+    
+    
+    
+
+void Start ()
+{
+
+}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
