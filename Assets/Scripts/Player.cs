468bd94c3a3f7723ae42de1946916bab5811ae97
Unknown <Cirieneve@users.noreply.github.com>
2018-03-23 15:24:38 -0600

    Made some new prefabs - killed unnecessary audio everywhere

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,94 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+
+public class Player : MonoBehaviour {
+
+    // Helps set gravity and jumpVelocity - note that time is in seconds
+    private float moveSpeed = 5;
+    private float minJumpHeight = 1;
+    private float maxJumpHeight = 5.5f;
+    private float timeToJumpApex = 0.65f;
+    private float accelerationTimeAirborne = 0.2f;
+    private float accelerationTimeGrounded = 0.1f;
+
+    // Equation assuming we're at the top of our jump should help explain gravity - jumpHeight = (gravity * timeToJumpApex**2)/2 so isolate gravity
+    // Equation for velocity is jumpVelocity = gravity * timeToJumpApex
+    private float gravity;
+    private float minJumpVelocity;
+    private float maxJumpVelocity;
+
+    private Vector2 directionalInput;
+    private Vector3 velocity;
+    private float velocityXSmoothing;
+
+    private Controller2D controller;
+
+    public void SetDirectionalInput(Vector2 input)
+    {
+        directionalInput = input;
+    }
+
+    public void OnJumpInputDown()
+    {
+        if (controller.collisions.below)
+        {
+            velocity.y = maxJumpVelocity;
+        }
+    }
+    public void OnJumpInputUp()
+    {
+        if (velocity.y > minJumpVelocity)
+        {
+            velocity.y = minJumpVelocity;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+
+        // Note that gravity has to be negative, hence the -1
+        gravity = (-1)*(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateVelocity();
+
+        controller.Move(velocity * Time.deltaTime, directionalInput);
+
+        // This makes our falling make more sense
+        // Due to how our gravity is implemented, this makes sure we don't just fall really quickly when we step off a platform
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+	}
+
+    void CalculateVelocity()
+    {
+        // Smoothing movement in x
+        float targetVelocityX = directionalInput.x * moveSpeed;
+
+        if (controller.collisions.below)
+        {
+
+            velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, accelerationTimeGrounded);
+        }
+        else
+        {
+            velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, accelerationTimeAirborne);
+        }
+
+        // Applying gravity to the player's velocity and moves accordingly
+        velocity.y += gravity * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/RaycastController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider2D))]
+public class RaycastController : MonoBehaviour {
+
+    // The layers we want to collide with
+    public LayerMask collisionMask;
+
+    // The amount we inset our raycasts by - we do not want to change this
+    public const float skinWidth = 0.010f;
+    const float distanceBetweenRays = 0.25f;
+
+    // The number of raycasts we have and the spacing
+    [HideInInspector] public int horizontalRayCount;
+    [HideInInspector] public int verticalRayCount;
+    [HideInInspector] public float horizontalRaySpacing;
+    [HideInInspector] public float verticalRaySpacing;
+
+    [HideInInspector] public CapsuleCollider2D collider;
+    [HideInInspector] public RaycastOrigins raycastOrigins;
+
+    // A struct that stores the places the raycasts come from
+    public struct RaycastOrigins
+    {
+        public Vector2 topLeft;
+        public Vector2 topRight;
+        public Vector2 bottomLeft;
+        public Vector2 bottomRight;
+    }
+
+    // Use this for initialization
+    public virtual void Start()
+    {
+        collider = GetComponent<CapsuleCollider2D>();
+        CalculateRaySpacing();
+    }
+
+    public void UpdateRaycastOrigins()
+    {
+        // Finds the bounds of the box collider and shrinks it slightly by the skinWidth
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    public void CalculateRaySpacing()
+    {
+        // Finds the bounds of the box collider and shrinks it slightly by the skinWidth
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        float boundsWidth = bounds.size.x;
+        float boundsHeight = bounds.size.y;
+
+        // Calculate number of rays based on distance between rays
+        horizontalRayCount = Mathf.RoundToInt(boundsHeight / distanceBetweenRays);
+        verticalRayCount = Mathf.RoundToInt(boundsWidth / distanceBetweenRays);
+
+        // Calculate ray spacing
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+}
