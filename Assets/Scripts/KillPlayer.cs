304346040fd92f919d6610f2a86b6d6192b876d7
alexczeto <alexczeto@yahoo.ca>
2018-03-28 19:31:37 -0600

    Audio Controller in

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -28,8 +28,7 @@ public class KillPlayer : MonoBehaviour {
         if (GetComponent<GrapplingHook>().currentNode)
         {
             animator.SetTrigger("Die");
-            GameObject.Find("GameController").GetComponent<AudioController>().playDeathSound();
-            //Invoke("TeleportPlayerToCheckpoint", delayTime);
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<AudioController>().playDeathSound();
             TeleportPlayerToCheckpoint();
         }
         else
