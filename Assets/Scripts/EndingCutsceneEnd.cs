f048507549141001db65d20d49e01d728874f7dc
Sonia Budac <budac@ualberta.ca>
2018-04-10 17:18:07 -0600

    Commit

---
+++ Assets/Scripts/EndingCutsceneEnd.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingCutsceneEnd : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //SceneManager.LoadScene("Opening Cutscene", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Opening Cutscene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
